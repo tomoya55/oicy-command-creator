7aab3f9c516c9b85168545343feeb86ec53d42e2
Tomoya Hirano <tomoya@cookpad.com>
2020-01-27 16:00:43 +0900

    Enable to return arbitarty data in Trigger

--- OicyResponse.ts
+++ OicyResponse.ts
@@ -12,6 +12,7 @@ export class OicyTrigger extends OicyResponse {
   linkText?: string
   readonly targetSubMrrKeys: TargetSubMrrKeys
   callback?: string
+  [key: string]: any
 
   /**
    * <b>!!PACKAGE PRIVATE!! DO NOT CALL THIS.</b>
